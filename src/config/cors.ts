d9decdc928bbd559ec298fabbb6516db4121bd29
Aryan Kumar <78974917+aryanda1@users.noreply.github.com>
2023-12-07 22:23:14 +0530

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,11 +1,9 @@
 import { CorsOptions } from "cors";
 
 const whiteList = [
-	'https://borrow-sphere-client.vercel.app',
-	'https://borrow-sphere-client.vercel.app/*',
-	"https://borrow-sphere.ary0n.fun",
-	"https://borrow-sphere.ary0n.fun/*"
-  ];
+    'https://borrow-sphere-client.vercel.app',
+    'https://borrow-sphere.ary0n.fun',
+];
 const corsOptions: CorsOptions = {
 	origin: function (origin, callback) {
 		if ((origin && whiteList.indexOf(origin) !== -1) || !origin) {
