c6072796980accd2111612e453392a4cce2ec9ce
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-12-12 14:29:54 +0530

    modified cors options.

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,9 +1,20 @@
 import { CorsOptions } from "cors";
 
-const whiteList = ["*"];
-const corsOptions: CorsOptions = {
-	origin: whiteList,
-	credentials: true, // This is important for requests with credentials
+var allowlist = [
+	"http://localhost:3000",
+	"https://borrow-sphere.ary0n.fun",
+	"https://borrow-sphere-client.vercel.app",
+];
+var corsOptions = function (req: any, callback: any) {
+	var corsOptions;
+	console.log(req.header("Origin"));
+	console.log(allowlist.indexOf(req.header("Origin")));
+	if (allowlist.indexOf(req.header("Origin")) !== -1) {
+		corsOptions = { origin: req.header("Origin"), credentials: true };
+	} else {
+		corsOptions = { origin: false };
+	}
+	callback(null, corsOptions);
 };
 
 export default corsOptions;
