7e81d24667ed4ebcafa979fb735934b7cd765870
Aryan Kumar <78974917+aryanda1@users.noreply.github.com>
2024-01-31 18:10:30 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -52,6 +52,11 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true"); // Set credentials to true
+	 if(req.method === 'OPTIONS') {
+        return res.status(200).json(({
+            body: "OK"
+        }))
+    }
   next();
 });
 
